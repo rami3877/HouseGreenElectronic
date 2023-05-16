3a95454591bb442142ae333a255723ff0ef192f8
Rami Mhawish <rami3877@gmail.com>
2023-05-16 04:28:01 +0300

    for Seting

--- green.ino
+++ green.ino
@@ -1,31 +1,106 @@
+#include <string.h>
+#include <EEPROM.h>
 #include <IRremote.hpp>
-
-
 #include <LiquidCrystal_I2C.h>
 #include "in.hpp"
 #define pinOfremote 2
 
+
+#define upkey    0xB946FF00
+#define downkey  0xEA15FF00
+#define leftkey  0xBC43FF00
+#define rightkey 0xBC43FF00
+
+
+
+
+
+
 IRrecv remote(pinOfremote);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+int hour[] = { 1, 0, 0 };
+int date[] = { 2023, 5, 1 };
+long int time;
+long int time1;
 
-long int tiem;
 void setup() {
-  tiem = millis();
+  time = millis();
+  time1 = millis();
+
   lcdinit();
   remote.enableIRIn();
 
   Serial.begin(9600);
   pinMode(3, 1);
+
+
+  lcd.setCursor(12, 0);
+  for (int x = 0; x < 2; x++) {
+
+    lcd.print(hour[x]);
+    if (x < 1)
+      lcd.print(":");
+  }
 }
-// 720
+
 bool isligth = true;
 void loop() {
-  if (millis() >= tiem + 2000) {
-    tiem = millis();
-    lcdClean(); 
+  if (millis() >= time1 + 1000) {
+    time1 = millis();
+    hour[2]++;
   }
+  if (millis() >= time + 2000) {
+    time = millis();
+    lcdClean();
+  }
+
+  lcd.setCursor(3, 0);
+  for (int x = 0; x < 3; x++) {
+
+    lcd.print(date[x]);
+    if (x < 2)
+      lcd.print("/");
+  }
+
+
+  lcd.setCursor(12, 0);
+  for (int x = 0; x < 2; x++) {
+    if (x == 1 && hour[1] < 10) lcd.print("0");
+    lcd.print(hour[x]);
+    if (x < 1)
+      lcd.print(":");
+  }
+
+
+  if (date[1] == 12) {
+    date[1] = 1;
+    date[0]++;
+  }
+
+
+  if (hour[0] == 12) {
+    hour[0] = 1;
+    if (date[2] < 30)
+      date[2]++;
+    else if (date[2] == 30) {
+      date[1]++;
+    } else date[2] = 1;
+  }
+  if (hour[1] == 60) {
+
+    hour[0]++;
+    hour[1] = 0;
+  }
+  if (hour[2] == 60) {
+    hour[1]++;
+    hour[2] = 0;
+  }
+
+
+
   if (remote.decode()) {
+
     if (remote.decodedIRData.decodedRawData == 0xE916FF00 /* key nubmer 1*/) {
       Serial.println("1");
       isligth = false;
@@ -41,7 +116,8 @@ void loop() {
 
       lcd.print("light Auto mod");
     }
-    // Serial.println(remote.decodedIRData.decodedRawData, HEX);
+
+    
 
     remote.resume();
   }
